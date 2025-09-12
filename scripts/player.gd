1c3c22be0978256b5d274e80b6605dcbc78d7184
pinky <pinkipandarojo@gmail.com>
2025-09-11 23:52:34 -0400

    Objects

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 var last_direction = "down"
 
+# Variables para interacción con objetos
+var carrying_object = null
+var can_pickup = false
+var pickup_area = null
+
 func _physics_process(_delta):
 	var dir = Vector2.ZERO
 
@@ -47,3 +52,29 @@ func _physics_process(_delta):
 		velocity = Vector2.ZERO
 
 	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("g"):
+		if carrying_object == null and can_pickup and pickup_area != null:
+			# Alzar el tomate
+			var tomato_scene = preload("res://scenes/Tomato.tscn")
+			carrying_object = tomato_scene.instantiate()
+			get_parent().add_child(carrying_object)
+			carrying_object.position = position + Vector2(0, -40)
+		elif carrying_object != null and can_pickup and pickup_area != null:
+			# Dejar el tomate en el mesón
+			carrying_object.position = pickup_area.global_position
+			carrying_object = null
+		elif carrying_object != null:
+			# Mantener el tomate junto al jugador
+			carrying_object.position = position + Vector2(0, -40)
+
+func _on_Area2D_body_entered(body):
+	if body == self:
+		can_pickup = true
+		pickup_area = get_node("..")
+
+func _on_Area2D_body_exited(body):
+	if body == self:
+		can_pickup = false
+		pickup_area = null
